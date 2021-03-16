2ef6fc11bca1c01e8a0d9f0e83740a44e6edb345
LethargicPuppy14 <LethargicPuppy14@gmail.com>
2021-03-16 17:07:42 -0500

    Fixed test stuff

---
+++ old src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="public/Star-Royalfred.html">Click me for Star-Royalfred</a>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Bill.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class Bill extends Component{
+    render(){
+        return(
+            <div>
+                <h1>The Star-Royalfred Act</h1>
+                <i>Proposed by Congressman Ruben of Illinois</i>
+                <p>This makes spamming bad</p>
+            </div>
+        )
+    }
+}
+
+export default Bill
